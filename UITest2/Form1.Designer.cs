6a57a2efdad2f5915038ed6ac1112f5a05101abf
Nam Nguyễn <nkn6294@live.co.uk>
2025-05-06 01:12:40 +0700

    Updated

--- UITest2/Form1.Designer.cs
+++ UITest2/Form1.Designer.cs
@@ -28,35 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.testButton = new System.Windows.Forms.Button();
+            this.testButtonSync = new System.Windows.Forms.Button();
+            this.testButtonAsync = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // testButton
+            // testButtonSync
             // 
-            this.testButton.Location = new System.Drawing.Point(359, 200);
-            this.testButton.Name = "testButton";
-            this.testButton.Size = new System.Drawing.Size(75, 23);
-            this.testButton.TabIndex = 0;
-            this.testButton.Text = "Test";
-            this.testButton.UseVisualStyleBackColor = true;
-            this.testButton.Click += new System.EventHandler(this.TestButton_Click);
+            this.testButtonSync.Location = new System.Drawing.Point(236, 36);
+            this.testButtonSync.Name = "testButtonSync";
+            this.testButtonSync.Size = new System.Drawing.Size(75, 23);
+            this.testButtonSync.TabIndex = 0;
+            this.testButtonSync.Text = "TestSync";
+            this.testButtonSync.UseVisualStyleBackColor = true;
+            this.testButtonSync.Click += new System.EventHandler(this.TestButtonSync_Click);
+            // 
+            // testButtonAsync
+            // 
+            this.testButtonAsync.Location = new System.Drawing.Point(420, 36);
+            this.testButtonAsync.Name = "testButtonAsync";
+            this.testButtonAsync.Size = new System.Drawing.Size(75, 23);
+            this.testButtonAsync.TabIndex = 1;
+            this.testButtonAsync.Text = "TestAsync";
+            this.testButtonAsync.UseVisualStyleBackColor = true;
+            this.testButtonAsync.Click += new System.EventHandler(this.TestButtonAsync_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(90, 127);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(595, 20);
+            this.resultTextBox.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.testButtonAsync);
+            this.Controls.Add(this.testButtonSync);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "TestAsync";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.Button testButtonSync;
+        private System.Windows.Forms.Button testButtonAsync;
+        private System.Windows.Forms.TextBox resultTextBox;
     }
 }
 
--- UITest2/Form1.cs
+++ UITest2/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,9 +19,49 @@ namespace UITest2
             InitializeComponent();
         }
 
-        private void TestButton_Click(object sender, EventArgs e)
+        private void TestButtonSync_Click(object sender, EventArgs e)
         {
+            //Thread.Sleep(5000);
+            //Console.WriteLine($"Click:{((Button)sender).Text}");
+            resultTextBox.Text = "Get data...";
+            //Thread.Sleep(1000);
+            string data = CallLongRunningApi("https://jsonplaceholder.typicode.com/todos/1");
+            resultTextBox.Text = data.Substring(0, Math.Min(500, data.Length));
+        }
 
+        private async void TestButtonAsync_Click(object sender, EventArgs e)
+        {
+            //await Task.Delay(5000);
+            //Console.WriteLine($"Click:{((Button)sender).Text}");
+            resultTextBox.Text = "Get data...";
+            string data = await CallLongRunningApiAsync("https://jsonplaceholder.typicode.com/todos/1");
+            resultTextBox.Text = data.Substring(0, Math.Min(500, data.Length));
+        }
+
+        private async Task<string> CallLongRunningApiAsync(string apiUrl)
+        {
+            using (var client = new HttpClient())
+            {
+                await Task.Delay(5000);
+                var response = await client.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        private string CallLongRunningApi(string apiUrl)
+        {
+            using (var client = new HttpClient())
+            {
+                var task = Task.Run(() => client.GetAsync(apiUrl));
+                Thread.Sleep(5000);
+                task.Wait();
+                var response = task.Result;
+                response.EnsureSuccessStatusCode();
+                var rTask = Task.Run(() => response.Content.ReadAsStringAsync());
+                rTask.Wait();
+                return rTask.Result;
+            }
         }
     }
 }
